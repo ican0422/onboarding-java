759d6cf30c95d2000297ae5d850284b5cfecd5fc
ican0422 <ican0422@gmail.com>
2025-01-18 18:09:34 +0900

    feat: @AllArgsConstructor 추가

--- src/main/java/com/careerthon/onboardingjava/domain/user/dto/request/UserSignRequestDto.java
+++ src/main/java/com/careerthon/onboardingjava/domain/user/dto/request/UserSignRequestDto.java
@@ -1,8 +1,10 @@
 package com.careerthon.onboardingjava.domain.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UserSignRequestDto {
     private String username;
     private String password;
