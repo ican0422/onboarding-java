41217f4719e86bbb1dea0c616c03851f820c7f6c
ican0422 <ican0422@gmail.com>
2025-01-18 18:16:42 +0900

    test: UserServiceTest 회원가입 실패 케이스 작성 (이미 가입한 유저의 대한 예외처리)

--- src/test/java/com/careerthon/onboardingjava/common/domain/user/service/UserServiceTest.java
+++ src/test/java/com/careerthon/onboardingjava/common/domain/user/service/UserServiceTest.java
@@ -6,6 +6,7 @@ import com.careerthon.onboardingjava.domain.user.entity.User;
 import com.careerthon.onboardingjava.domain.user.entity.UserRole;
 import com.careerthon.onboardingjava.domain.user.repository.UserRepository;
 import com.careerthon.onboardingjava.domain.user.service.UserService;
+import com.sun.jdi.request.InvalidRequestStateException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,8 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -56,5 +56,33 @@ public class UserServiceTest {
         assertEquals(username, response.getUsername());
         assertEquals(nickname, response.getNickname());
         assertEquals(UserRole.ROLE_USER, response.getAuthorities().getAuthorityName());
+    }
+
+    @Test
+    public void 이미_가입한_유저_예외처리_테스트() {
+        // given
+        // dto 값 설정
+        String username = "testUsername";
+        String password = "testPassword";
+        String nickname = "testNickname";
+
+        // dto 생성
+        UserSignupRequestDto request = new UserSignupRequestDto(username, password, nickname);
+
+        // 유저 만들기
+        long userId = 1L;
+        User user = new User(request, UserRole.ROLE_USER);
+        ReflectionTestUtils.setField(user, "id", userId);
+
+        // 이미 가입 되어 있는지 확인
+        given(userRepository.existsByUsername(username)).willReturn(true);
+
+        // when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            userService.signup(request);
+        });
+
+        // then
+        assertEquals("이미 가입된 회원입니다.", exception.getMessage());
     }
 }
