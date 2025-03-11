9784f51df324aa19504e2599a11247052864a58d
ican0422 <ican0422@gmail.com>
2025-03-12 00:08:20 +0900

    feat: Enum값에 문자열 추가

--- src/main/java/com/careerthon/onboardingjava/domain/user/entity/UserRole.java
+++ src/main/java/com/careerthon/onboardingjava/domain/user/entity/UserRole.java
@@ -1,9 +1,18 @@
 package com.careerthon.onboardingjava.domain.user.entity;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum UserRole {
-    ROLE_USER,
-    ROLE_ADMIN;
+    ROLE_USER(Authority.USER),
+    ROLE_ADMIN(Authority.ADMIN);
+
+    private final String userRole;
+
+    public static class Authority{
+        public static final String USER = "USER";
+        public static final String ADMIN = "ADMIN";
+    }
 }
