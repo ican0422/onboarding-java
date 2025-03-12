4919332b0976c65bf4f98cc1bea7f3c0116e8da9
ican0422 <ican0422@gmail.com>
2025-03-13 01:50:09 +0900

    feat: 구글 ID 추가 (무조건 필수는 아님)

--- src/main/java/com/careerthon/onboardingjava/domain/user/entity/User.java
+++ src/main/java/com/careerthon/onboardingjava/domain/user/entity/User.java
@@ -22,15 +22,19 @@ public class User {
     @Column(nullable = true, unique = true)
     private Long kakaoId;
 
+    @Column(nullable = true, unique = true)
+    private Long googleId;
+
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
     @Builder
-    public User(String username, String nickname, String password, UserRole role, Long kakaoId) {
+    public User(String username, String nickname, String password, UserRole role, Long kakaoId, Long googleId) {
         this.username = username;
         this.nickname = nickname;
         this.password = password;
         this.kakaoId = kakaoId;
+        this.googleId = googleId;
         this.role = role;
     }
 }
